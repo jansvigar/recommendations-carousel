d7ecdf4e422e8e3837dadfe4ca92e1224590ffe7
Jan Vigar <jansvigar@gmail.com>
2018-12-22 15:46:48 -0800

    make basic slider function (next and previous) work

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,12 @@
 import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import Typography from "@material-ui/core/Typography";
+
 import Carousel from "./Carousel";
 
 function App() {
   return (
     <React.Fragment>
       <CssBaseline />
-      <Typography variant="h4" gutterBottom>
-        Top Recommendations For You
-      </Typography>
       <Carousel />
     </React.Fragment>
   );
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,44 +1,69 @@
 import React, { Component } from "react";
 import { withStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import CardItem from "./CardItem";
 import { API_URL } from "../config";
 
 const styles = theme => ({
   root: {
-    flexGrow: 1
+    width: "866px",
+    margin: "auto"
   }
 });
 
 class Carousel extends Component {
   state = {
     items: [],
-    showing: [],
-    page: 1
+    currentIdx: 0,
+    length: 4,
+    maxLength: 16
   };
   componentDidMount() {
     fetch(`${API_URL}/users/1/items?amt=16`)
       .then(res => res.json())
       .then(json => this.setState({ items: json.items }));
   }
+  handlePrevious = () => {
+    this.setState(prevState => {
+      const newIdx = prevState.currentIdx - prevState.length;
+      return newIdx >= 0 ? { currentIdx: newIdx } : prevState;
+    });
+  };
+  handleNext = () => {
+    const { maxLength } = this.state;
+    this.setState(prevState => {
+      const newIdx = prevState.currentIdx + prevState.length;
+      return newIdx < maxLength ? { currentIdx: newIdx } : prevState;
+    });
+  };
   render() {
     const { classes } = this.props;
+    const { currentIdx, length, items } = this.state;
+    const showing = items.filter((item, index) => {
+      return index >= currentIdx && index < currentIdx + length;
+    });
     return (
       <Grid container className={classes.root} spacing={16}>
         <Grid item xs={12}>
+          <Typography variant="h4" gutterBottom>
+            Top Recommendations For You
+          </Typography>
           <Grid
             container
             className={classes.list}
             justify="center"
             spacing={16}
           >
-            {this.state.items.map(item => (
+            {showing.map(item => (
               <Grid key={item.id} item>
                 <CardItem item={item} />
               </Grid>
             ))}
           </Grid>
         </Grid>
+        <button onClick={this.handlePrevious}>Previous</button>
+        <button onClick={this.handleNext}>Next</button>
       </Grid>
     );
   }
